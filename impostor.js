042f985e04c644fe1742f58f1adeca23c21a6044
Sophia Balico <sophiabalico.tds2.senai@gmail.com>
2024-11-14 16:58:59 -0300

    feat: criação das classes

---
+++ impostor.js
@@ -0,0 +1,9 @@
+import Aluno from './aluno.js';
+
+class Impostor extends Aluno{
+    constructor(nome, grupo, funcao) {
+        this.nome = nome;
+        this.grupo = grupo;
+        this.funcao = funcao;
+    }
+}
